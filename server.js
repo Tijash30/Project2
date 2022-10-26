056fa7f3c72eef18e32a6f740914bd64e1b2cc1c
Tijash30 <113260010+Tijash30@users.noreply.github.com>
2022-10-25 23:12:01 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+var listInfo = [{title: "John", description: 20}, {title: "Mary", description: 30}];
+
+
+app.get('/blogs', (req, res) => {
+    res.render('pages/index', {
+        listInfo: listInfo,
+    });
+})
+
+app.post('/postblog' , (req, res) => {
+    const title = req.body.title;
+    const description = req.body.description;
+
+    listInfo.push({title: title, description: description})
+
+    res.render('pages/index', {listInfo: listInfo});
+})
+
+app.post('/editblog' , (req, res) => {
+    const title = req.body.title;
+    const description = req.body.description;
+
+    for (var i = 0; i < listInfo.length; i++) {
+        if (listInfo[i].title == title) {
+            listInfo[i].description = description;          
+
+            res.render('pages/index', {listInfo: listInfo});
+        }
+    }
+    //res.send(title + description)
+})
+
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
